7d6c0d41bfadb759920bb01c340ceb2378406229
Karl Marx Lopez <karlmarx.webdev@gmail.com>
2020-09-06 19:18:21 +0800

    chore: assets folder

--- rnassessment/babel.config.js
+++ rnassessment/babel.config.js
@@ -7,6 +7,7 @@ module.exports = {
         root: ['.'],
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
+          '@app/assets': './src/assets',
           '@app/core-ui': './src/core-ui',
           '@app/components': './src/components',
           '@app/screens': './src/screens',
