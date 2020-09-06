0043508bc5339cb7dfd2181517727b32736330f7
Karl Marx Lopez <karlmarx.webdev@gmail.com>
2020-09-07 01:33:20 +0800

    fix(core-ui): change placeholder color

--- src/core-ui/inputs/Input.tsx
+++ src/core-ui/inputs/Input.tsx
@@ -2,6 +2,7 @@ import React, {FC} from 'react';
 import {View, StyleSheet, TextInput} from 'react-native';
 import {InputProps} from './type';
 import {scale} from 'react-native-size-matters';
+import {Colors} from '@app/themes';
 
 export const Input: FC<InputProps> = ({
   containerStyle,
@@ -9,14 +10,19 @@ export const Input: FC<InputProps> = ({
   ...inputProps
 }) => (
   <View style={[styles.container, containerStyle]}>
-    <TextInput style={[styles.textInput, style]} {...inputProps} />
+    <TextInput
+      style={[styles.textInput, style]}
+      placeholderTextColor={Colors.gray}
+      {...inputProps}
+    />
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomWidth: scale(1),
     padding: scale(10),
+    borderColor: Colors.lightGray,
   },
   textInput: {
     fontSize: scale(16),
